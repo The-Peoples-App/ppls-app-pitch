1d17f70decbee486b2ea9a81f490b74fc7a985b9
tjupp <travisjupp@gmail.com>
2026-04-23 08:11:01 -0600

    test(revealjs): data-background-image double encoding
    - Disabling image optimization for local development since reavealjs
      double encodes the url causing the image to not load on localhost
      (plenty of reports on this on official reveal repo)
    
    - Compare the images (optimized) dimensions and filesize after this push
      (pushing unoptimized) to the server to see the cost of not using
      optimized background image in the background of a reavealjs slide.
    
    - Current (optimized) rendered peoples guide image source in production:
      size: 478kb
      dimensions: 2308 x 1366
      format: image/webp
    
    - Peoples guide image properties after this push? (TODO)
      size: changed???
      dimensions: shouldn't change.
      format: probably the same.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,19 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    // Force Astro to use Sharp explicitly for local development
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {},
+    },
+  },
   vite: {
+    server: {
+      fs: {
+        allow: ['.', './src/assets/images'],
+      },
+    },
     ssr: {
       noExternal: ['beercss'],
     }
