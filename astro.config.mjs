a7a5ebc731e49be5837d6fce2edda4d47c305960
tjupp <travisjupp@gmail.com>
2026-04-29 08:19:33 -0600

    build(astro): configure SSR
    Context: Configure SSR w/ Netlify adapter to unblock basic
    authentication with Astro Middleware.
    
    Action:
    - Configure/Install astrojs/netlify adapter
    - Set output to `server` in Astro config
    
    Result: Astro Middleware password gate implementation (to follow) task
    unblocked with astrojs/netlify adapter & SSR configured.
    
    Related: #4 > #20

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
+import netlifyIntegration from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlifyIntegration(),
   image: {
     // Force Astro to use Sharp explicitly for local development
     service: {
